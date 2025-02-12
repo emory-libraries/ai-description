2cdbfb9f62feaea84433108eb8fd9b01cde35291
Nico <ntyjeski@amazon.com>
2025-02-12 23:24:15 +0000

    Return newline

--- projects/infra/modules/iam/main.tf
+++ projects/infra/modules/iam/main.tf
@@ -143,6 +143,7 @@ resource "aws_iam_role_policy_attachment" "ecs_task_execution_policy_attach" {
   role       = aws_iam_role.ecs_task_execution_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
+
 resource "aws_iam_role_policy_attachment" "ecs_task_execution_ecr_policy" {
   role       = aws_iam_role.ecs_task_execution_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
