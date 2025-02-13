f7ccd5c2eff19403489cea7ab91f56fd000ad355
EC2 Default User <ec2-user@ip-10-10-122-183.ec2.internal>
2025-02-13 20:12:57 +0000

    remove comments

--- lib/src/image_captioning_assistant/data/data_classes.py
+++ lib/src/image_captioning_assistant/data/data_classes.py
@@ -9,32 +9,6 @@ from image_captioning_assistant.data.constants import BiasLevel, BiasType
 from pydantic import BaseModel, Field
 
 
-# class PotentialBias(BaseModel):
-#     """A potential bias present in an image."""
-
-#     bias_type: BiasType = Field(..., description="The type of bias exibited by the image")
-#     bias_level: BiasLevel = Field(..., description="The level of bias exibited by the image")
-#     explanation: str = Field(..., description="Freeform commentary on the bias recognized in the image")
-
-
-# BiasAnalysis = List[PotentialBias]
-
-
-# class StructuredMetadata(BaseModel):
-#     """Metadata fields for an image.
-
-#     This is the metadata requested for items in the Langmuir collection.
-#     """
-
-#     description: str = Field(..., description="A description of the contents of an image")
-#     transcription: str = Field(..., description="A transcription of any text in the image")
-#     people_and_groups: list[str] = Field(..., description="The names of any people or groups recognized in the image")
-#     date: str = Field(..., description="The approximate date the image may have been taken")
-#     location: str = Field(..., description="The approximate location where the image may have been taken")
-#     publication_info: str = Field(..., description="Any known publication information")
-#     contextual_info: str = Field(..., description="Any known contextual information around the image")
-#     potential_biases: BiasAnalysis = Field(..., description="A list of any potential biases present in the image")
-
 class Transcription(BaseModel):
     print: List[str] = Field(
         ...,
