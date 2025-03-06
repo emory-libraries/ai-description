0fd52fa7d32617493048a7377407281058fb66d8
Nico <ntyjeski@amazon.com>
2025-03-06 01:46:11 +0000

    Remove uninteresting outputs

--- projects/infra/outputs.tf
+++ projects/infra/outputs.tf
@@ -3,6 +3,11 @@
 
 # Main outputs
 
+output "cloudfront_url" {
+  description = "URL of Cloudfront"
+  value       = module.cloudfront.distribution_domain_name
+}
+
 output "api_gateway_invoke_url" {
   description = "The URL of the API Gateway endpoint"
   value       = module.api_gateway.api_gateway_invoke_url
@@ -13,11 +18,6 @@ output "uploads_bucket_name" {
   value       = module.s3.uploads_bucket_name
 }
 
-output "works_table_name" {
-  description = "The name of the DynamoDB table"
-  value       = module.dynamodb.works_table_name
-}
-
 output "ecs_cluster_name" {
   description = "The name of the ECS cluster"
   value       = module.ecs.cluster_name
@@ -26,17 +26,4 @@ output "ecs_cluster_name" {
 output "lambda_function_names" {
   description = "The names of the Lambda functions"
   value       = module.lambda.function_names
-}
-
-output "cloudfront_url" {
-  description = "URL of Cloudfront"
-  value       = module.cloudfront.distribution_domain_name
-}
-
-output "target_function_name" {
-  value = module.lambda.function_arns["authorize"]
-}
-
-output "authorizer_uri" {
-  value = module.lambda.invoke_arns["authorize"]
 }
