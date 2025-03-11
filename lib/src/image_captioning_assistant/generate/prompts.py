83756a516e2b878bfe4c29e7feacbec2ff1a13cb
jaynmar <jaynmar@amazon.com>
2025-03-11 14:02:50 +0000

    pre-commit

--- lib/src/image_captioning_assistant/generate/prompts.py
+++ lib/src/image_captioning_assistant/generate/prompts.py
@@ -1,9 +1,10 @@
 # Copyright © Amazon.com and Affiliates: This deliverable is considered Developed Content as defined in the AWS Service
 # Terms and the SOW between the parties dated 2025.
+import json
 import os
 
 from jinja2 import Template
-import json
+
 import image_captioning_assistant.data.data_classes as dc
 
 COT_TAG_NAME = "object_detail_and_bias_analysis"
@@ -13,11 +14,7 @@ COT_TAG_END = f"</{COT_TAG_NAME}>"
 current_dir = os.path.dirname(__file__)
 
 # dump metadata definition for prompt
-METADATA_MODEL_DUMP = json.dumps(
-    dc.Metadata.model_json_schema(),
-    indent=4,
-    ensure_ascii=False
-)
+METADATA_MODEL_DUMP = json.dumps(dc.Metadata.model_json_schema(), indent=4, ensure_ascii=False)
 
 with open(os.path.join(current_dir, "system_prompt.txt"), "r") as file:
     system_prompt = file.read()
@@ -28,8 +25,7 @@ with open(os.path.join(current_dir, "system_prompt_court_order.txt"), "r") as fi
 
 with open(os.path.join(current_dir, "user_prompt_metadata.txt"), "r") as file:
     user_prompt_metadata = Template(file.read(), autoescape=True).render(
-        COT_TAG=COT_TAG, COT_TAG_END=COT_TAG_END, COT_TAG_NAME=COT_TAG_NAME
-        , PYDANTIC_MODEL_DUMP=METADATA_MODEL_DUMP
+        COT_TAG=COT_TAG, COT_TAG_END=COT_TAG_END, COT_TAG_NAME=COT_TAG_NAME, PYDANTIC_MODEL_DUMP=METADATA_MODEL_DUMP
     )
 
 
