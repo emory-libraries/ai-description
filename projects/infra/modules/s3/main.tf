a304d3d6974ae1f52edd08227a0586378624d98c
Nicolas Tyjeski <ntyjeski@amazon.com>
2025-04-02 11:02:40 -0400

    Simplify uploads lifecycle

--- projects/infra/modules/s3/main.tf
+++ projects/infra/modules/s3/main.tf
@@ -131,21 +131,11 @@ resource "aws_s3_bucket_lifecycle_configuration" "uploads_lifecycle" {
   bucket = aws_s3_bucket.uploads.id
 
   rule {
-    id     = "transition-to-ia-and-glacier"
+    id     = "expire-old-uploads"
     status = "Enabled"
 
-    transition {
-      days          = 30
-      storage_class = "STANDARD_IA"
-    }
-
-    transition {
-      days          = 90
-      storage_class = "GLACIER"
-    }
-
     expiration {
-      days = 365
+      days = 90
     }
   }
 }
